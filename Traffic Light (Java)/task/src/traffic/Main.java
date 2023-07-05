80c90f1860305aadcb4566fa8c08f2db1e407e6f
joel0134 <joeldavethu@gmail.com>
2023-07-05 17:05:52 +0800

    finished the first stage

---
+++ Traffic Light (Java)/task/src/traffic/Main.java
@@ -0,0 +1,17 @@
+package traffic;
+
+public class Main {
+	public static void main(String[] args) {
+		printControlPanel();
+	}
+
+	static void printControlPanel() {
+		System.out.println("Welcome to the traffic management system!");
+
+		System.out.println("Menu:");
+		System.out.println("1. Add");
+		System.out.println("2. Delete");
+		System.out.println("3. System");
+		System.out.println("0. Quit");
+	}
+}
